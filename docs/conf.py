896b896f459878bef0d284eb7df33416c775faf8
timendum <timedum@gmail.com>
2025-01-15 10:50:10 +0100

    Sort imports in docs

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import importlib.metadata
 import os
 import sys
-import importlib.metadata
 
 sys.path.insert(0, os.path.abspath(".."))
 
